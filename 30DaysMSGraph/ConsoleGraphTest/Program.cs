9e5713dabb691fa5fc68cd243f11534ba2c238ab
Stephan Graber <stephan.graber@students.ffhs.ch>
2019-06-28 15:55:25 +0200

    Program.cs LoadConf

--- 30DaysMSGraph/ConsoleGraphTest/Program.cs
+++ 30DaysMSGraph/ConsoleGraphTest/Program.cs
@@ -15,5 +15,34 @@ namespace ConsoleGraphTest
         {
             Console.WriteLine("Hello World!");
         }
+        private static GraphServiceClient _graphServiceClient;
+        private static HttpClient _httpClient;
+
+        private static IConfigurationRoot LoadAppSettings()
+        {
+            try
+            {
+                var config = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+                // Validate required settings
+                if (string.IsNullOrEmpty(config["applicationId"]) ||
+                    string.IsNullOrEmpty(config["applicationSecret"]) ||
+                    string.IsNullOrEmpty(config["redirectUri"]) ||
+                    string.IsNullOrEmpty(config["tenantId"]) ||
+                    string.IsNullOrEmpty(config["domain"]))
+                {
+                    return null;
+                }
+
+                return config;
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                return null;
+            }
+        }
     }
 }
